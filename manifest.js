b551e2001978bac28d00944558ef7beb7ae3982a
= <salviolorenzo@gmail.com>
2019-04-09 20:38:17 -0400

    adding basic files

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+  "name": "Stack Overflow Extension",
+  "version": "1.0",
+  "description": "An extension for developers to search Stack Overflow with fewer clicks.",
+  "manifest_version": 2,
+  "background": {
+    "scripts": ["./background.js"],
+    "persistent": false
+  }
+}
